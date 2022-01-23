fd2ce64b18dec8f859ae756780c6cefd15d1f045
AdamPalmer28 <93833080+AdamPalmer28@users.noreply.github.com>
2022-01-23 17:26:20 +0000

    Test
    Test

---
+++ test.py
@@ -0,0 +1,57 @@
+# test
+
+
+#%% Properties
+import time
+import numpy as np
+
+def timer(func):
+    def wrapper(*args):
+        before = time.time()
+        val = func(*args)
+        print("Function tooks:", np.round(time.time() - before,4), " seconds")
+        return val
+    return wrapper
+
+@timer
+def test_run(p):
+    sum_val = 0
+    for i in range(10**p+1):
+        sum_val += i
+    return sum_val
+
+print(test_run(6))
+#%%
+test_list = []
+for i in range(10):
+    test_list.append(i)
+    
+    if i == 6:
+        result = test_list
+
+#print(result)
+
+# %% speed test
+
+
+array = np.zeros(64,dtype='byte') 
+
+array[45]== 1
+array[63]== 1
+before = time.time()
+for i in range (100000):
+    for i in array.nonzero():
+        2*i
+array = (time.time()-before)
+
+
+before2 = time.time()
+
+list_test = [45,63]
+for i in range (100000):
+    for i in list_test:
+        2*i
+list_time = (time.time()-before2)
+
+print(list_time - array)
+
